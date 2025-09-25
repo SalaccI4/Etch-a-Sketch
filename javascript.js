adfd242d44005b3a81fea8e56aa8d7d3a4f8fff9
SalaccI4 <66245600+SalaccI4@users.noreply.github.com>
2025-09-25 16:05:51 -0700

    Add hover feature that changes div colors in grid

--- javascript.js
+++ javascript.js
@@ -1,8 +1,17 @@
+let box
+
 const contain = document.querySelector(".container")
 
 
 for (let i = 0; i < 256; i++){
-    let box = document.createElement("div")
+    box = document.createElement("div")
     box.classList.add("boxes")
     contain.appendChild(box)
 }
+
+contain.addEventListener("mouseover", function (e) {
+    const r = Math.floor(Math.random() * 256)
+    const g = Math.floor(Math.random() * 256)
+    const b = Math.floor(Math.random() * 256)
+    e.target.style.backgroundColor = `rgb(${r}, ${g}, ${b})`
+})
